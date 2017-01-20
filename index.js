2973f492d187e4c983504a09bd98ede3e7155796
Antério Vieira <anteriovieira@gmail.com>
2017-01-20 13:36:15 -0200

    Updated

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import segment from './src/segment.js';
-import compileHtml from './src/compileHtml.js';
+import compileHtml from './src/compile-html.js';
 
 angular.module('ngSegmentTree', [])
     .component.apply(angular, segment)
