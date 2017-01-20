19bd04edeeaf52333443babe70cfdf8205da6fb1
Antério Vieira <anteriovieira@gmail.com>
2017-01-20 13:35:49 -0200

    Updated

--- rollup.config.js
+++ rollup.config.js
@@ -5,8 +5,7 @@ import template from 'rollup-plugin-template';
 export default {
   entry: 'index.js',
   format: 'cjs',
-  //dest: path.resolve(__dirname, 'dist/angular-segment-tree.js'),
-  dest: 'C:/Projetos/PCF/PCF/PCF/Scripts/AngularJs/angular-segment-tree.js',
+  dest: path.resolve(__dirname, 'dist/angular-segment-tree.js'),
   plugins: [ 
       template({include: 'src/templates/*.html'}),
       babel() 
